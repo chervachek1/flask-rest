f67a33a7e34d0047065d09cbbaa916ed652deab3
chervachek1 <kusmichandrij@gmail.com>
2023-09-15 03:00:59 +0300

    add category, redone relationship, add many-to-many

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 from flask import Flask, request, jsonify
 from flask_sqlalchemy import SQLAlchemy
-from flask_marshmallow import Marshmallow
+from marshmallow import Schema, fields
 import os
 
 # Init app
@@ -11,8 +11,10 @@ app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + os.path.join(basedir, 'db
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 # Init db
 db = SQLAlchemy(app)
-# Init ma
-ma = Marshmallow(app)
+product_categories = db.Table('product_categories',
+                              db.Column('product_id', db.Integer, db.ForeignKey('product.id')),
+                              db.Column('category_id', db.Integer, db.ForeignKey('category.id'))
+                              )
 
 
 # Product Class/Model
@@ -22,39 +24,43 @@ class Product(db.Model):
     description = db.Column(db.String(200))
     price = db.Column(db.Float)
     qty = db.Column(db.Integer)
+    categories = db.relationship("Category", backref="categories", secondary=product_categories)
 
-    def __init__(self, name, description, price, qty):
-        self.name = name
-        self.description = description
-        self.price = price
-        self.qty = qty
+
+# Category Class/Model
+class Category(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(100), unique=True)
+    products = db.relationship("Product", backref="products", secondary=product_categories)
 
 
 # Product Schema
-class ProductSchema(ma.Schema):
-    class Meta:
-        fields = ('id', 'name', 'description', 'price', 'qty')
+class PlainProductSchema(Schema):
+    id = fields.Int(dump_only=True)
+    name = fields.Str(required=True)
+    description = fields.Str(required=True)
+    price = fields.Float(required=True)
+    qty = fields.Int(required=True)
+
+
+class PlainCategorySchema(Schema):
+    id = fields.Int(dump_only=True)
+    name = fields.Str(required=True)
+
+
+class ProductSchema(PlainProductSchema):
+    categories = fields.List(fields.Nested(PlainCategorySchema()), dump_only=True)
+
+
+class CategorySchema(PlainCategorySchema):
+    products = fields.List(fields.Nested(PlainProductSchema()), dump_only=True)
 
 
 # Init schema
 product_schema = ProductSchema()
 products_schema = ProductSchema(many=True)
-
-
-# Create a Product
-@app.route('/product', methods=['POST'])
-def add_product():
-    name = request.json['name']
-    description = request.json['description']
-    price = request.json['price']
-    qty = request.json['qty']
-
-    new_product = Product(name, description, price, qty)
-
-    db.session.add(new_product)
-    db.session.commit()
-
-    return product_schema.jsonify(new_product)
+category_schema = CategorySchema()
+categories_schema = CategorySchema(many=True)
 
 
 # Get All Products
@@ -62,44 +68,7 @@ def add_product():
 def get_products():
     all_products = Product.query.all()
     result = products_schema.dump(all_products)
-    return jsonify(result.data)
-
-
-# Get Single Products
-@app.route('/product/<id>', methods=['GET'])
-def get_product(id):
-    product = Product.query.get(id)
-    return product_schema.jsonify(product)
-
-
-# Update a Product
-@app.route('/product/<id>', methods=['PUT'])
-def update_product(id):
-    product = Product.query.get(id)
-
-    name = request.json['name']
-    description = request.json['description']
-    price = request.json['price']
-    qty = request.json['qty']
-
-    product.name = name
-    product.description = description
-    product.price = price
-    product.qty = qty
-
-    db.session.commit()
-
-    return product_schema.jsonify(product)
-
-
-# Delete Product
-@app.route('/product/<id>', methods=['DELETE'])
-def delete_product(id):
-    product = Product.query.get(id)
-    db.session.delete(product)
-    db.session.commit()
-
-    return product_schema.jsonify(product)
+    return jsonify(result)
 
 
 # Run Server
